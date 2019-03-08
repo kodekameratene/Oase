4568a3eb9416334e03c3858b9eb39714a197d2dd
Henry S. Sjøen <henryskorpe@gmail.com>
2019-03-08 13:16:26 +0100

    Simple testing of main skeleton
    This just checks that the app builds, and triggers a frame.
    Also it checks that the 3 main buttons exists with the correct icon and do something when tapped.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,26 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:jonah/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Main sceleton smoke test', (WidgetTester tester) async {
+    // This just checks that the app builds, and triggers a frame.
+    // Also it checks that the 3 main buttons exists with the correct icon and do something when tapped.
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the 'event' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.event));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Tap the 'info' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.info));
+    await tester.pump();
+
+    // Tap the 'news' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.message));
+    await tester.pump();
   });
 }
