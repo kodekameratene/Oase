8d1b3680ef2b40194097b7ff48cfe222cc38150c
Henry Sjøen <henry@kodekameraten.no>
2019-05-11 18:03:57 +0200

    init pages

--- lib/screens/ProgramPage.dart
+++ lib/screens/ProgramPage.dart
@@ -2,26 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:jonah/src/ContentPost.dart';
 
 class ProgramPage extends StatelessWidget {
-  List<ContentPost> _events = events;
+  final List<ContentPost> _events = events;
 
   Widget build(context) {
     return ListView(
-        children: _events
-            .map(
-              (post) => ListTile(
-                    leading: Text(
-                        "${post.startTime.month.toString().padLeft(2, '0')}/"
+      children: _events
+          .map(
+            (post) => ListTile(
+                  leading: Text(
+                    "${post.startTime.month.toString().padLeft(2, '0')}/"
                         "${post.startTime.day.toString().padLeft(2, '0')}"
-                        "\n${post.startTime.hour}:${post.startTime.minute}"),
-                    title: Text(post.title),
-                    subtitle: Text(post.content),
-//                    trailing: Image.network(
-//                      'http://kodekameraten.no/img/kodekameraten.jpg',
-//                      height: 100,
-//                      width: 100,
-//                    ),
+                        "\n${post.startTime.hour}:${post.startTime.minute}",
                   ),
-            )
-            .toList());
+                  title: Text(post.title),
+                  subtitle: Text(
+                    post.content,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  trailing: Image.network(
+                    post.img,
+                    height: 100,
+                    width: 100,
+                  ),
+                  onTap: () => showDialog(
+                        context: context,
+                        child: Dialog(
+                          child: Column(
+                            children: <Widget>[
+                              Text(
+                                post.title,
+                                textScaleFactor: 2,
+                              ),
+                              Text(
+                                post.content,
+                                textScaleFactor: 1.8,
+                                textAlign: TextAlign.justify,
+                              )
+                            ],
+                          ),
+                        ),
+                        barrierDismissible: true,
+                      ),
+                ),
+          )
+          .toList(),
+    );
   }
 }
--- lib/src/ContentPost.dart
+++ lib/src/ContentPost.dart
@@ -12,18 +12,19 @@ class ContentPost {
   final int importance;
   final bool hidden;
 
-  ContentPost({this.title,
-    this.content="",
-    this.img,
-    this.showCategories,
-    this.startTime,
-    this.endTime,
-    this.number,
-    this.url,
-    this.location,
-    this.category,
-    this.importance,
-    this.hidden});
+  ContentPost(
+      {this.title,
+      this.content = "",
+      this.img = "",
+      this.showCategories,
+      this.startTime,
+      this.endTime,
+      this.number,
+      this.url,
+      this.location,
+      this.category,
+      this.importance,
+      this.hidden});
 }
 
 final events = [
@@ -34,7 +35,7 @@ final events = [
   new ContentPost(
     title: "Skolene er åpne",
     content:
-    "Etter at du har registrert deg kan du legge fra deg bagasjen din der du skal sove.",
+        "Etter at du har registrert deg kan du legge fra deg bagasjen din der du skal sove.",
     startTime: DateTime(2019, 01, 25, 18, 00),
     endTime: DateTime(2019, 01, 25, 19, 33),
   ),
@@ -45,8 +46,9 @@ final events = [
   new ContentPost(
     title: "Celebration",
     content:
-    "Møtene på Impuls kalles Celebration. Her bruker vi god tid til lovsang, tale og det er også mulighet for forbønn.",
+        "Møtene på Impuls kalles Celebration. Her bruker vi god tid til lovsang, tale og det er også mulighet for forbønn.",
     startTime: DateTime(2019, 01, 25, 20, 00),
+    img: "http://kodekameraten.no/img/hello.jpg",
   ),
   //Lørdag
   new ContentPost(
@@ -56,8 +58,9 @@ final events = [
   new ContentPost(
     title: "Celebration",
     content:
-    "Møtene på Impuls kalles Celebration. Her bruker vi god tid til lovsang, tale og det er også mulighet for forbønn.",
+        "Møtene på Impuls kalles Celebration. Her bruker vi god tid til lovsang, tale og det er også mulighet for forbønn.",
     startTime: DateTime(2019, 01, 26, 10, 00),
+    img: "http://kodekameraten.no/img/hello.jpg",
   ),
   new ContentPost(
       title: "Seminarbolk",
@@ -65,8 +68,7 @@ final events = [
       showCategories: ["c1", 'c2'],
       startTime: DateTime(2019, 01, 26, 12, 00),
       endTime: DateTime(2019, 01, 26, 13, 00),
-      category: "s1"
-  ),
+      category: "s1"),
 ];
 
 final newsPosts = [
@@ -79,11 +81,13 @@ final newsPosts = [
     title: "This should be hidden!",
     content: "Lorem ipsum dolor sit amet.",
     startTime: DateTime.now(),
+    img: "http://kodekameraten.no/img/hello.jpg",
     hidden: true,
   ),
   new ContentPost(
     title: "Velkommen til Jonah!",
     content: "Lorem ipsum dolor sit amet.",
+    img: "http://kodekameraten.no/img/hello.jpg",
     startTime: DateTime.now(),
   ),
 ];
@@ -102,5 +106,6 @@ final infoPosts = [
   new ContentPost(
     title: "Velkommen til Jonah!",
     content: "Lorem ipsum dolor sit amet.",
+    img: "http://kodekameraten.no/img/hello.jpg",
   )
 ];
