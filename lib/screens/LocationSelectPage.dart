d5de96bc40de4888d841f28eab469886898c9f62
Henry Sjøen <henry@kodekameraten.no>
2019-05-25 01:18:50 +0200

    init the SelectLocation page
    Also; Made some mock-data

---
+++ lib/screens/LocationSelectPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:jonah/src/ContentPost.dart';
+import 'package:jonah/styles.dart';
+
+class LocationSelectPage extends StatelessWidget {
+  final List<Location> _locations = locations;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Velg sted"), backgroundColor: Styles.primaryColor),
+            body: ListView(
+                children: _locations
+                    .map((post) => ListTile(
+                          contentPadding: EdgeInsets.only(
+                            left: 20,
+                            top: 20,
+                            right: 20,
+                            bottom: 20,
+                          ),
+                          title: Text(post.title),
+                          subtitle: Text(post.startTime.toLocal().toString()),
+                          trailing: Image.network(
+                            post.img,
+                            height: 100,
+                            width: 100,
+                          ),
+                          onTap: () => AlertDialog(
+                                backgroundColor: Styles.primaryColor,
+                                title: Text(post.title),
+                                content: Text("Select location"),
+                              ),
+                        ))
+                    .toList())));
+  }
+}
