228fb99a9a08afe4868a06543517285e8f4cd698
Maria Osen <marose42@gmail.com>
2019-06-21 17:06:06 +0200

    #20 add zoom functionality

--- lib/widgets/molecules/fullScreenImage.dart
+++ lib/widgets/molecules/fullScreenImage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:oase/styles.dart';
 
 class FullScreenPage extends StatelessWidget {
   FullScreenPage({this.img});
@@ -7,6 +8,21 @@ class FullScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Styles.colorPrimary,
+          automaticallyImplyLeading: true,
+          leading: IconButton(icon:Icon(Icons.arrow_back),
+            onPressed:() => Navigator.pop(context, false),
+          )
+      ),
+      body: Center(
+        child: image(context),
+      ),
+    );
+  }
+
+  Widget image(context) {
     return GestureDetector(
         child: Hero(
           tag: 'imageCVP',
@@ -14,9 +30,9 @@ class FullScreenPage extends StatelessWidget {
             imageProvider: NetworkImage(this.img),
           ),
         ),
-        onHorizontalDragDown: (details) {
-          Navigator.pop(context);
-        }
+        //onHorizontalDragDown: (details) {
+        //  Navigator.pop(context);
+        //}
     );
   }
 }
