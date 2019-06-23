5c3a4820df87af6bb09b03558afa6b8cdd581e7c
Henry Skorpe Sjøen <henryskorpe@gmail.com>
2019-06-24 01:16:17 +0200

    almost working daydividerlabels (#44)
    Need to look over how we draw our layout since we have some bugs...
    
    When starting drawing the list from the bottom, the divider gets put in the wrong place.

--- lib/pages/ProgramPage.dart
+++ lib/pages/ProgramPage.dart
@@ -1,20 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:oase/helpers/asset_helpers.dart';
 import 'package:oase/styles.dart';
 import 'package:oase/widgets/organisms/KokaCardEvent.dart';
 
 import 'ContentViewerPage.dart';
+import 'package:oase/widgets/organisms/DayButton.dart';
+import 'package:oase/widgets/molecules/WeekWidget.dart';
+import 'package:oase/helpers/date_helper.dart';
 
-class ProgramPage extends StatelessWidget {
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return KokaCardEvent(
-        document: document,
-        onTapAction: () => Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ContentViewerPage(document))));
-  }
+class ProgramPage extends StatefulWidget {
+  @override
+  _ProgramPageState createState() => _ProgramPageState();
+}
+
+class _ProgramPageState extends State<ProgramPage> {
+  List<DayButton> list = new List<DayButton>();
+  List<String> days = new List<String>();
+  List<String> daySeperators = new List<String>();
 
   Widget build(context) {
     return Material(
@@ -24,22 +28,102 @@ class ProgramPage extends StatelessWidget {
           title: AssetHelpers.getAppBarImage(),
           centerTitle: true,
         ),
-        body: StreamBuilder(
-            stream: Firestore.instance
-                .collection('festival/G0OHb6fOBJEcLv4bUsvX/content')
-                .where("page", arrayContains: 'program')
-                .orderBy("startTime")
-                .snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData)
-                return Center(child: Text("Laster inn data..."));
-              return ListView.builder(
-                  itemCount: snapshot.data.documents.length,
-                  itemBuilder: (context, index) =>
-                      _buildListItem(context, snapshot.data.documents[index]));
-            }),
+        body: Column(
+          children: <Widget>[
+            // Container(height: 60, child: dayButtonList()),
+            Flexible(
+              child: programList(),
+            ),
+          ],
+        ),
         backgroundColor: Styles.colorBackgroundColorMain,
       ),
     );
   }
+
+  Widget _buildProgramListItem(
+      BuildContext context, DocumentSnapshot document) {
+    String dayNumberFromTimestamp =
+        getDayNumberFromTimestamp(document["startTime"]);
+    bool shouldShowNewDayLabel = false;
+    if (!days.contains(dayNumberFromTimestamp)) {
+      days.add(dayNumberFromTimestamp);
+      shouldShowNewDayLabel = true;
+    }
+    String dayName = getFullDayFromTimestamp(document["startTime"]);
+    return (Column(
+      children: <Widget>[
+        if (shouldShowNewDayLabel)
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Text(
+              dayName,
+              style: Styles.textEventCardHeader,
+            ),
+          ),
+        KokaCardEvent(
+            document: document,
+            onTapAction: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ContentViewerPage(document)),
+                ))
+      ],
+    ));
+  }
+
+  Widget programList() {
+     return (StreamBuilder(
+        stream: Firestore.instance
+        .collection('festival/G0OHb6fOBJEcLv4bUsvX/content')
+        .where("page", arrayContains: 'program')
+        .orderBy("startTime")
+        .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData)
+            return Center(
+                child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text("Laster inn data...")));
+          days = [];
+          daySeperators = new List<String>();
+          return ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (context, index) {
+                return _buildProgramListItem(
+                    context, snapshot.data.documents[index]);
+              });
+        }));
+  }
+
+
+  // Widget _buildButtonListItem(BuildContext context, DocumentSnapshot document) {
+  //   // watch_your_profanity
+  //   return DayButton(
+  //       date: getDayNumberFromTimestamp(document['startTime']),
+  //       day: getDayFromTimestamp(document["startTime"]));
+  // }
+
+  // Widget dayButtonList() {
+  //   return (StreamBuilder(
+  //       stream: instance,
+  //       builder: (context, snapshot) {
+  //         if (!snapshot.hasData)
+  //           return Center(child: Text("Laster inn data..."));
+  //         days = [];
+  //         return ListView.builder(
+  //             scrollDirection: Axis.horizontal,
+  //             itemCount: snapshot.data.documents.length,
+  //             itemBuilder: (context, index) {
+  //               String dayNumberFromTimestamp = getDayNumberFromTimestamp(
+  //                   snapshot.data.documents[index]["startTime"]);
+  //               if (!days.contains(dayNumberFromTimestamp)) {
+  //                 days.add(dayNumberFromTimestamp);
+  //                 return _buildButtonListItem(
+  //                     context, snapshot.data.documents[index]);
+  //               }
+  //               return SizedBox.shrink();
+  //             });
+  //       }));
+  // }
 }
