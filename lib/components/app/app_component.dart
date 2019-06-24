b4596f761823cd7492bd95cee496f6b4d8968484
Henry Sjøen <henry@kodekameraten.no>
2019-06-25 00:58:05 +0200

    PUSH NOTIFICATIONS WITH SUBSCRIPTION TO VoksenOase TOPIC

--- lib/components/app/app_component.dart
+++ lib/components/app/app_component.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,42 @@ class AppComponent extends StatefulWidget {
 }
 
 class AppComponentState extends State<AppComponent> {
+  String _homeScreenText = "Waiting for token...";
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+//        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+//        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+//        _navigateToItemDetail(message);
+      },
+    );
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _homeScreenText = "Push Messaging token: $token";
+      });
+      _firebaseMessaging.subscribeToTopic('VoksenOase');
+      print(_homeScreenText);
+    });
+  }
+
   AppComponentState() {
     final router = new Router();
     Routes.configureRoutes(router);
