7404f0128239ecd1725640e0069523846cd81358
Henry Skorpe Sjøen <henryskorpe@gmail.com>
2019-06-25 23:34:00 +0200

    proper logic for when to show day-divider-labels (#50)

--- lib/pages/ProgramPage.dart
+++ lib/pages/ProgramPage.dart
@@ -2,13 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:oase/helpers/asset_helpers.dart';
+import 'package:oase/helpers/date_helper.dart';
 import 'package:oase/styles.dart';
+import 'package:oase/widgets/organisms/DayButton.dart';
 import 'package:oase/widgets/organisms/KokaCardEvent.dart';
 
 import 'ContentViewerPage.dart';
-import 'package:oase/widgets/organisms/DayButton.dart';
-import 'package:oase/widgets/molecules/WeekWidget.dart';
-import 'package:oase/helpers/date_helper.dart';
 
 class ProgramPage extends StatefulWidget {
   @override
@@ -42,14 +41,7 @@ class _ProgramPageState extends State<ProgramPage> {
   }
 
   Widget _buildProgramListItem(
-      BuildContext context, DocumentSnapshot document) {
-    String dayNumberFromTimestamp =
-        getDayNumberFromTimestamp(document["startTime"]);
-    bool shouldShowNewDayLabel = false;
-    if (!days.contains(dayNumberFromTimestamp)) {
-      days.add(dayNumberFromTimestamp);
-      shouldShowNewDayLabel = true;
-    }
+      BuildContext context, DocumentSnapshot document, shouldShowNewDayLabel) {
     String dayName = getFullDayFromTimestamp(document["startTime"]);
     return (Column(
       children: <Widget>[
@@ -73,12 +65,12 @@ class _ProgramPageState extends State<ProgramPage> {
   }
 
   Widget programList() {
-     return (StreamBuilder(
+    return (StreamBuilder(
         stream: Firestore.instance
-        .collection('festival/G0OHb6fOBJEcLv4bUsvX/content')
-        .where("page", arrayContains: 'program')
-        .orderBy("startTime")
-        .snapshots(),
+            .collection('festival/G0OHb6fOBJEcLv4bUsvX/content')
+            .where("page", arrayContains: 'program')
+            .orderBy("startTime")
+            .snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData)
             return Center(
@@ -90,40 +82,49 @@ class _ProgramPageState extends State<ProgramPage> {
           return ListView.builder(
               itemCount: snapshot.data.documents.length,
               itemBuilder: (context, index) {
-                return _buildProgramListItem(
-                    context, snapshot.data.documents[index]);
+                bool shouldShowNewDayLabel = index == 0 ? true : false;
+                if (index > 0) {
+                  int lastOne = index - 1;
+                  if (getDayNumberFromTimestamp(
+                          snapshot.data.documents[lastOne]["startTime"]) !=
+                      getDayNumberFromTimestamp(
+                          snapshot.data.documents[index]["startTime"])) {
+                    shouldShowNewDayLabel = true;
+                  }
+                }
+                return _buildProgramListItem(context,
+                    snapshot.data.documents[index], shouldShowNewDayLabel);
               });
         }));
   }
 
+// Widget _buildButtonListItem(BuildContext context, DocumentSnapshot document) {
+//   // watch_your_profanity
+//   return DayButton(
+//       date: getDayNumberFromTimestamp(document['startTime']),
+//       day: getDayFromTimestamp(document["startTime"]));
+// }
 
-  // Widget _buildButtonListItem(BuildContext context, DocumentSnapshot document) {
-  //   // watch_your_profanity
-  //   return DayButton(
-  //       date: getDayNumberFromTimestamp(document['startTime']),
-  //       day: getDayFromTimestamp(document["startTime"]));
-  // }
-
-  // Widget dayButtonList() {
-  //   return (StreamBuilder(
-  //       stream: instance,
-  //       builder: (context, snapshot) {
-  //         if (!snapshot.hasData)
-  //           return Center(child: Text("Laster inn data..."));
-  //         days = [];
-  //         return ListView.builder(
-  //             scrollDirection: Axis.horizontal,
-  //             itemCount: snapshot.data.documents.length,
-  //             itemBuilder: (context, index) {
-  //               String dayNumberFromTimestamp = getDayNumberFromTimestamp(
-  //                   snapshot.data.documents[index]["startTime"]);
-  //               if (!days.contains(dayNumberFromTimestamp)) {
-  //                 days.add(dayNumberFromTimestamp);
-  //                 return _buildButtonListItem(
-  //                     context, snapshot.data.documents[index]);
-  //               }
-  //               return SizedBox.shrink();
-  //             });
-  //       }));
-  // }
+// Widget dayButtonList() {
+//   return (StreamBuilder(
+//       stream: instance,
+//       builder: (context, snapshot) {
+//         if (!snapshot.hasData)
+//           return Center(child: Text("Laster inn data..."));
+//         days = [];
+//         return ListView.builder(
+//             scrollDirection: Axis.horizontal,
+//             itemCount: snapshot.data.documents.length,
+//             itemBuilder: (context, index) {
+//               String dayNumberFromTimestamp = getDayNumberFromTimestamp(
+//                   snapshot.data.documents[index]["startTime"]);
+//               if (!days.contains(dayNumberFromTimestamp)) {
+//                 days.add(dayNumberFromTimestamp);
+//                 return _buildButtonListItem(
+//                     context, snapshot.data.documents[index]);
+//               }
+//               return SizedBox.shrink();
+//             });
+//       }));
+// }
 }
