3893757f1d3a80c9168f6128b813c8ad5f600bfd
Henry Sjøen <henry@kodekameraten.no>
2019-07-02 00:15:46 +0200

    .

--- lib/components/app/app_component.dart
+++ lib/components/app/app_component.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:oase/helpers/appInfo_helper.dart';
 
 import '../../config/application.dart';
 import '../../config/routes.dart';
@@ -58,7 +59,7 @@ class AppComponentState extends State<AppComponent> {
   Widget build(BuildContext context) {
     final app = new MaterialApp(
       initialRoute: '/',
-      title: 'Oase',
+      title: AppInfo.appName,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: Application.router.generator,
     );
